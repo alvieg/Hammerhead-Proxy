7a669e43ca584ee9cc82ac2e6f926d848b03db71
HammerHead <hammerhead.contact2025@gmail.com>
2025-09-18 17:22:16 -0500

    revert

--- public/script.js
+++
@@ -1,8 +0,0 @@
-function run() {
-    let iframeWindow = document.getElementById("iframeWindow");
-    let url = "https://hammerhead-games.vercel.app";
-
-    iframeWindow.src = __uv$config.prefix + __uv$config.encodeUrl(url);
-};
-
-run()
